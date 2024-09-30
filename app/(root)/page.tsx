39c285d454a45cf267be2cbb13059d18d5e60352
data-dude42 <ss8820788@gmail.com>
2024-09-30 03:22:17 -0400

    implement the Headerbox, TotalBalanceBox.tsx, AnimatedCounter.tsx components and add the various initial functionalaties

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,29 @@
-import React from 'react'
-
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 const Home = () => {
+  const loggedIn = { firstName: 'Paras Singh' };
   return (
-    <section className='home'>
-    <div className='home-content'>
-      <header className='home-header'> Welcome aboard, Paras, from Orion Bank
-      </header>
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+
+        </header>
+      </div>
     </section>
-    
+
   )
 }
-
 export default Home
 
