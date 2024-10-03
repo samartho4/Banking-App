5cf4bf6889e59f373a06d128fffea40f15ee6686
data-dude42 <ss8820788@gmail.com>
2024-10-03 10:53:13 -0400

    implement complete DoughnutChart.tsx and TotalBalanceBox.tsx

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -5,8 +5,28 @@ import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const DoughnutChart = () => {
-    return <Doughnut data={[]} />
+const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+    const data = {
+        datasets: [
+            {
+                label: 'Banks',
+                data: [1250, 2500, 3750],
+                backgroundColor: ['#4B0082', '#191970']
+            }
+        ],
+        labels: ['Bank 1', 'Bank 2']
+    }
+    return <Doughnut
+        data={data}
+        options={{
+            cutout: '60%',
+            plugins: {
+                legend: {
+                    display: false
+                }
 
+            }
+        }}
+    />
 }
 export default DoughnutChart
